93f41f7db175fbc3a6d070fb50e81868c9cef6bf
joeeoj <joeeoj@blueskyemail.net>
2021-04-12 19:16:43 -0700

    reformat build script slightly (mostly the same)

--- generate.py
+++ generate.py
@@ -1,12 +1,29 @@
-#!/usr/bin/python3
-import os, json, shutil
-if os.path.exists("build"):
-    shutil.rmtree("build")
-os.mkdir("build")
-shutil.copytree("recipes", "build/recipes")
-shutil.copytree("images", "build/images")
-shutil.copy2("index.html", "build")
-shutil.copy2("recipe.html", "build")
-shutil.copy2("stylesheet.css", "build")
-f = open("build/recipelist.json", "w")
-f.write(json.dumps(os.listdir("recipes")))
+#!/usr/bin/env python3
+import json
+import os
+from pathlib import Path
+import shutil
+
+
+root = Path.cwd() / 'build'
+
+if os.path.exists(root):
+    shutil.rmtree(root)
+
+# make build dir
+os.mkdir(root)
+
+# copy folder
+shutil.copytree('recipes', root / 'recipes')
+shutil.copytree('images', root / 'images')
+
+# copy individual files
+shutil.copy2('index.html', root)
+shutil.copy2('recipe.html', root)
+shutil.copy2('stylesheet.css', root)
+
+# create json of recipes for nav-list
+with open(root / 'recipelist.json', 'w') as f:
+    # filter out dumb .DS_Store
+    recipes = [fname for fname in os.listdir('recipes') if not fname.startswith('.')]
+    f.write(json.dumps(recipes))
